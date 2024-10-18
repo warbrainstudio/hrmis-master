f57473201667747ddad4470f9842d3872dcf9a51
warbrainstudio <aduystudio@gmail.com>
2024-10-18 13:59:30 +0700

    add absen

---
+++ application/models/AbsenModel.php
@@ -0,0 +1,196 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class AbsenModel extends CI_Model
+{
+  private $_table = 'absen_pegawai';
+  private $_tableView = '';
+
+
+  public function getQuery($filter = null)
+  {
+    $query = "
+      SELECT t.* FROM (
+        SELECT 
+          ab.*, 
+          
+          (CASE WHEN ab.status = 0 THEN 'Masuk' ELSE 'Pulang' END) AS nama_status,
+          (CASE WHEN ab.verified = 1 THEN 'Finger' ELSE 'Input' END) AS verifikasi
+        FROM absen_pegawai ab
+        
+      ) t
+      WHERE 1=1
+    ";
+
+    if (!is_null($filter)) $query .= $filter;
+    return $query;
+  }
+
+  public function getnullpegawaiQuery($filter = null)
+  {
+    $query = "
+        SELECT t.* FROM (
+          SELECT
+            ab.absen_id, 
+            COUNT(ab.tanggal_absen) AS datetime_count
+          FROM absen_pegawai ab
+          LEFT JOIN pegawai p ON ab.absen_id = p.absen_pegawai_id
+          WHERE p.absen_pegawai_id IS NULL
+          GROUP BY ab.absen_id
+        ) t
+         WHERE 1=1
+    ";
+    if (!is_null($filter)) $query .= $filter;
+    return $query;
+  }
+
+  public function getNull(){
+    $query = "
+          SELECT
+            absen_pegawai.absen_id 
+          FROM absen_pegawai
+          LEFT JOIN pegawai ON absen_pegawai.absen_id = pegawai.absen_pegawai_id
+          WHERE pegawai.absen_pegawai_id IS NULL
+          GROUP BY absen_pegawai.absen_id
+    ";
+    
+    // Assuming you have a database connection and using CodeIgniter's query builder
+    $result = $this->db->query($query);
+    return $result->result();
+  }
+
+
+
+  public function getAll($params = array(), $orderField = null, $orderBy = 'asc')
+  {
+      if (isset($params['tanggal_absen'])) {
+          
+        $orderField = 'tanggal_absen';
+        $dateParam = $params['tanggal_absen'];
+
+        $this->db->select('absen_pegawai.absen_id, 
+                          absen_pegawai.tanggal_absen,
+                          TO_CHAR(absen_pegawai.tanggal_absen, \'HH24:MI:SS\') AS jam_absen,
+                          CASE WHEN absen_pegawai.verified = 1 THEN \'Finger\' ELSE \'Input\' END AS verifikasi, 
+                          CASE WHEN absen_pegawai.status = 0 THEN \'Masuk\' ELSE \'Pulang\' END AS nama_status');
+        
+
+        if (preg_match('/^\d{4}-\d{2}$/', $dateParam)) {
+
+          list($year, $month) = explode('-', $dateParam);
+          $startDate = date('Y-m-d', strtotime("$year-$month-21 -1 month"));
+          $endDate = date('Y-m-d', strtotime("$year-$month-21"));
+          $this->db->where("tanggal_absen BETWEEN '$startDate' AND '$endDate'");
+
+        }elseif (preg_match('/^\d{4}$/', $dateParam)) {
+
+          $startDate = date('Y-m-d', strtotime("$dateParam-01-01"));
+          $endDate = date('Y-m-d', strtotime("$dateParam-12-31"));
+          $this->db->where("tanggal_absen BETWEEN '$startDate' AND '$endDate'");
+
+        }else{  
+
+          $this->db->where("DATE(absen_pegawai.tanggal_absen)", $dateParam);
+
+        }
+
+        unset($params['tanggal_absen']);
+        
+        if (!is_null($orderField)) {
+          $this->db->order_by($orderField, $orderBy);
+        }
+    
+        return $this->db->get($this->_table)->result();
+
+      }elseif(isset($params['absen_pegawai_id'])){
+        
+        $orderField = 'tanggal_absen';
+
+        $this->db->select('absen_pegawai.absen_id, 
+                TO_CHAR(absen_pegawai.tanggal_absen, \'YYYY-MM-DD\') AS tanggal,
+                TO_CHAR(absen_pegawai.tanggal_absen, \'HH24:MI:SS\') AS jam_absen,
+                CASE WHEN absen_pegawai.verified = 1 THEN \'Finger\' ELSE \'Input\' END AS verifikasi, 
+                CASE WHEN absen_pegawai.status = 0 THEN \'Masuk\' ELSE \'Pulang\' END AS nama_status');
+        
+        $this->db->where($params);
+
+        if (!is_null($orderField)) {
+          $this->db->order_by($orderField, $orderBy);
+        }
+        
+        return $this->db->get($this->_table)->result();
+
+      }else{
+
+        $this->db->where($params);
+
+        if (!is_null($orderField)) {
+          $this->db->order_by($orderField, $orderBy);
+        }
+        
+        return $this->db->get($this->_table)->result();
+
+      }
+  }
+   
+
+  public function getDetail($params = array())
+  {
+    return $this->db->where($params)->get($this->_table)->row();
+  }
+
+  public function delete($id)
+  {
+    $response = array('status' => false, 'data' => 'No operation.');
+
+    try {
+      $this->db->delete($this->_table, array('id' => $id));
+
+      $response = array('status' => true, 'data' => 'Data has been deleted.');
+    } catch (\Throwable $th) {
+      $response = array('status' => false, 'data' => 'Failed to delete your data.');
+    };
+
+    return $response;
+  }
+
+  public function delete_pegawai($absen_id)
+  {
+    $response = array('status' => false, 'data' => 'No operation.');
+
+    try {
+      $this->db->delete($this->_table, array('absen_id' => $absen_id));
+
+      $response = array('status' => true, 'data' => 'Data has been deleted.');
+    } catch (\Throwable $th) {
+      $response = array('status' => false, 'data' => 'Failed to delete your data.');
+    };
+
+    return $response;
+  }
+
+  public function truncate()
+  {
+    $response = array('status' => false, 'data' => 'No operation.');
+
+    try {
+      $this->db->truncate($this->_table);
+
+      $response = array('status' => true, 'data' => 'Data has been truncated.');
+    } catch (\Throwable $th) {
+      $response = array('status' => false, 'data' => 'Failed to truncate your data.');
+    };
+
+    return $response;
+  }
+
+  function br2nl($text)
+  {
+    return str_replace("\r\n", '<br/>', htmlspecialchars_decode($text));
+  }
+
+  function clean_number($number)
+  {
+    return preg_replace('/[^0-9]/', '', $number);
+  }
+}
---
+++ application/modules/absen/controllers/Absen.php
@@ -0,0 +1,134 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+require_once(APPPATH . 'controllers/AppBackend.php');
+
+use alhimik1986\PhpExcelTemplator\PhpExcelTemplator;
+use alhimik1986\PhpExcelTemplator\setters\CellSetterArrayValueSpecial;
+
+define('SPECIAL_ARRAY_TYPE', CellSetterArrayValueSpecial::class);
+
+class Absen extends AppBackend
+{
+  function __construct()
+  {
+    parent::__construct();
+    $this->load->model(array(
+      'AppMixModel',
+      //'AppModel',
+      'AbsenModel',
+      'PegawaiModel'
+    ));
+    $this->load->library('form_validation');
+  }
+
+  public function index()
+  {
+    $data = array(
+      'app' => $this->app(),
+      'main_js' => $this->load_main_js('absen'),
+      'card_title' => 'Attendance Log',
+    );
+    $this->template->set('title', $data['card_title'] . ' | ' . $data['app']->app_name, TRUE);
+    $this->template->load_view('index', $data, TRUE);
+    $this->template->render();
+  }
+
+  public function ajax_get_all()
+  {
+    $filter = $this->input->get('filter');
+    $query = $this->AbsenModel->getQuery($filter);
+    $response = $this->AppMixModel->getdata_dtAjax($query);
+    echo json_encode($response);
+  }
+
+  public function ajax_get_pegawai_item()
+  {
+    $this->handle_ajax_request();
+    $ref = $this->input->get('attendancelog_id');
+    $response = $this->AbsenModel->getAll(['attendancelog_id' => $ref], 'asc');
+    echo json_encode($response);
+  }
+
+  public function ajax_save($id = null)
+  {
+    $this->handle_ajax_request();
+    $this->form_validation->set_rules($this->AbsenModel->rules());
+
+    if ($this->form_validation->run() === true) {
+      if (is_null($id)) {
+        echo json_encode($this->AbsenModel->insert());
+      } else {
+        echo json_encode($this->AbsenModel->update($id));
+      };
+    } else {
+      $errors = validation_errors('<div>- ', '</div>');
+      echo json_encode(array('status' => false, 'data' => $errors));
+    };
+  }
+
+  public function ajax_delete($id)
+  {
+    $this->handle_ajax_request();
+    echo json_encode($this->AbsenModel->delete($id));
+  }
+
+  public function ajax_delete_pegawai($absen_pegawai_id)
+  {
+    $this->handle_ajax_request();
+    echo json_encode($this->AbsenModel->deletepegawai($absen_pegawai_id));
+  }
+
+  public function excel()
+  {
+      try {
+          $date = $this->input->get('date');
+          $status = '';
+          $formattedDate = $date;
+          if (DateTime::createFromFormat('Y-m-d', $date) !== false) {
+              $fileTemplate = FCPATH . 'directory/templates/template-attendance-harian.xlsx';
+              $dateTime = DateTime::createFromFormat('Y-m-d', $date);
+              $Day = $dateTime->format('D');
+              $DayNumber = $dateTime->format('d');
+              $monthNumber = $dateTime->format('m');
+              $year = $dateTime->format('Y');
+              $formattedDay = $this->get_day($Day);
+              $formattedMonth = $this->get_month($monthNumber);
+              $formattedDate = $formattedDay.', '.$DayNumber.' '.$formattedMonth . ' ' . $year;
+              $status = $formattedDate;
+          } else if(DateTime::createFromFormat('Y-m', $date) !== false) {
+              $fileTemplate = FCPATH . 'directory/templates/template-attendance.xlsx';
+              $dateTime = DateTime::createFromFormat('Y-m', $date);
+              $monthNumber = $dateTime->format('m');
+              $year = $dateTime->format('Y');
+              $formattedMonth = $this->get_month($monthNumber);
+              $formattedDate = $formattedMonth . ' ' . $year;
+              $status = 'Bulan ' . $formattedDate;
+          } else{
+              ini_set('memory_limit', '4G');
+              $fileTemplate = FCPATH . 'directory/templates/template-attendance.xlsx';
+              $status = 'Tahun'; 
+          }
+          $callbacks = array();
+
+          $payload = $this->AbsenModel->getAll(array('tanggal_absen' => $date));
+  
+          if (!is_null($payload)) {
+
+              $outputFileName = 'histori absen ' . $formattedDate . '.xlsx';
+              $payloadStatic = $this->arrayToSetterSimple(array('tanggal_absen' => $formattedDate,'status' => $status));
+              $payloadStatic = array_merge($payloadStatic, $this->arrayToSetterSimple(array('app_export_date' => date('Y-m-d H:i:s'))));
+              $payload = $this->arrayToSetter($payload);
+              $payload = array_merge($payload, $payloadStatic);
+  
+              PhpExcelTemplator::outputToFile($fileTemplate, $outputFileName, $payload, $callbacks);
+          } else {
+              show_404();
+          }
+      } catch (\Throwable $th) {
+          log_message('error', $th->getMessage());
+  
+          show_error('Terjadi kesalahan ketika memproses data. Detail: ' . $th->getMessage(), 500);
+      }
+  }
+
+}
