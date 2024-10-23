d48ced5783d613b7dc745314b4aa88e12cf1a686
warbrainstudio <aduystudio@gmail.com>
2024-10-23 14:53:41 +0700

    add form cuti

---
+++ application/models/CutiModel.php
@@ -0,0 +1,337 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class CutiModel extends CI_Model
+{
+  private $_table = 'cuti';
+  private $_tableView = '';
+
+  public function rules()
+  {
+    return array(
+      [
+        'field' => 'tanggal_pengajuan',
+        'label' => 'Tanggal Pengajuan',
+        'rules' => 'required|trim'
+      ],
+      [
+        'field' => 'pegawai_id',
+        'label' => 'Pegawai',
+        'rules' => 'required|trim'
+      ],
+      [
+        'field' => 'jenis_cuti',
+        'label' => 'Jenis Cuti',
+        'rules' => 'required|trim'
+      ],
+    );
+  }
+
+  public function getQuery($filter = null)
+  {
+    $query = "
+      SELECT t.* FROM (
+        SELECT 
+            c.*,
+            pg.nrp,
+            pg.nama_lengkap,
+            kp.nama_kategori_pegawai,
+            kp.mkg,
+            jp.nama_jenis_pegawai,
+            u.kode_unit,
+            u.nama_unit,
+            su.kode_sub_unit,
+            su.nama_sub_unit,
+            j.kode_jabatan,
+            j.nama_jabatan,
+            tu.kode_tenaga_unit,
+            tu.nama_tenaga_unit,
+            (CASE WHEN pg.status_active = 1 THEN 'Aktif' ELSE 'Tidak Aktif' END) AS nama_status_active
+        FROM cuti c
+        JOIN pegawai pg ON pg.id = c.pegawai_id
+        LEFT JOIN kategori_pegawai kp ON kp.id = pg.kategori_pegawai_id
+        LEFT JOIN jenis_pegawai jp ON jp.id = pg.jenis_pegawai_id
+        LEFT JOIN unit u ON u.id = pg.unit_id
+        LEFT JOIN sub_unit su ON su.id = pg.sub_unit_id
+        LEFT JOIN jabatan j ON j.id = pg.jabatan_id
+        LEFT JOIN tenaga_unit tu ON tu.id = pg.tenaga_unit_id
+      ) t
+      WHERE 1=1
+    ";
+    if (!is_null($filter)) $query .= $filter;
+    return $query;
+  }
+
+  public function getAll($params = array(), $orderField = null, $orderBy = 'asc')
+  {
+    $this->db->where($params);
+
+    if (!is_null($orderField)) {
+      $this->db->order_by($orderField, $orderBy);
+    };
+
+    return $this->db->get($this->_table)->result();
+  }
+
+  public function getDetail($params = array())
+  {
+    $this->db->select('
+      cuti.*,
+      pegawai.nrp,
+      pegawai.nama_lengkap,
+      pegawai.jenis_kelamin,
+      pegawai.alamat_ktp,
+      pegawai.no_hp,
+      kategori_pegawai.nama_kategori_pegawai,
+      kategori_pegawai.mkg,
+      jenis_pegawai.nama_jenis_pegawai,
+      status_kontrak.nama_status_kontrak,
+      unit.kode_unit,
+      unit.nama_unit,
+      sub_unit.kode_sub_unit,
+      sub_unit.nama_sub_unit,
+      jabatan.kode_jabatan,
+      jabatan.nama_jabatan,
+      tenaga_unit.kode_tenaga_unit,
+      tenaga_unit.nama_tenaga_unit
+    ');
+    $this->db->from('cuti');
+    $this->db->join('pegawai', 'pegawai.id = cuti.pegawai_id');
+    $this->db->join('kategori_pegawai', 'kategori_pegawai.id = pegawai.kategori_pegawai_id', 'left');
+    $this->db->join('jenis_pegawai', 'jenis_pegawai.id = pegawai.jenis_pegawai_id', 'left');
+    $this->db->join('status_kontrak', 'status_kontrak.id = pegawai.status_kontrak_id', 'left');
+    $this->db->join('unit', 'unit.id = pegawai.unit_id', 'left');
+    $this->db->join('sub_unit', 'sub_unit.id = pegawai.sub_unit_id', 'left');
+    $this->db->join('jabatan', 'jabatan.id = pegawai.jabatan_id', 'left');
+    $this->db->join('tenaga_unit', 'tenaga_unit.id = pegawai.tenaga_unit_id', 'left');
+    $this->db->where($params);
+    return $this->db->get()->row();
+  }
+
+  public function insert()
+  {
+    $response = array('status' => false, 'data' => 'No operation.');
+    $jenis_detail = $this->input->post('jenis_cuti_detail');
+    $jenis_cuti = '';
+    if(!empty($jenis_detail)){
+      $jenis_cuti = $jenis_detail;
+    }else{
+      $jenis_cuti = $this->input->post('jenis_cuti');
+    }
+    try {
+      $this->jenis_cuti = $jenis_cuti;
+      $this->tanggal_pengajuan = $this->input->post('tanggal_pengajuan');
+      $this->pegawai_id = $this->input->post('pegawai_id');
+      $this->awal_cuti = $this->input->post('awal_cuti');
+      $this->akhir_cuti = $this->input->post('akhir_cuti');
+      $this->tanggal_bekerja = $this->input->post('tanggal_bekerja');
+      $this->alamat_cuti = $this->input->post('alamat_cuti');
+      $this->telepon_cuti = $this->input->post('telepon_cuti');
+      $this->created_by = $this->session->userdata('user')['id'];
+      $this->db->insert($this->_table, $this);
+
+      $response = array('status' => true, 'data' => 'Data has been saved.');
+    } catch (\Throwable $th) {
+      $response = array('status' => false, 'data' => 'Failed to save your data.');
+    };
+
+    /*$awalCuti = $this->input->post('awalcuti');
+    $akhirCuti = $this->input->post('akhircuti');
+    $awalTimestamp = strtotime($awalCuti);
+    $akhirTimestamp = strtotime($akhirCuti);
+
+    // Check if the dates are valid
+    if ($awalTimestamp !== false && $akhirTimestamp !== false && $awalTimestamp <= $akhirTimestamp) {
+        $currentTimestamp = $awalTimestamp;
+        $attendancelog_tes = 'attendancelog';
+        while ($currentTimestamp <= $akhirTimestamp) {
+            $currentDate = date('Y-m-d', $currentTimestamp);
+            $data = [
+              'absen_id' => $this->input->post('absen_pegawai_id'),
+              'tanggal_absen' => $currentDate,
+              'status' => "3",
+              'created_by' => $this->session->userdata('user')['id']
+            ];
+            $this->db->insert($attendancelog_tes, $data);
+            $currentTimestamp = strtotime("+1 day", $currentTimestamp);
+        }
+    } else {
+      $response = array('status' => false, 'data' => 'Invalid Date Range');
+    }*/
+
+
+    return $response;
+  }
+
+  public function insertBatch($data)
+  {
+    $response = array('status' => false, 'data' => 'No operation.');
+
+    try {
+      $this->db->insert_batch($this->_table, $data);
+
+      $response = array('status' => true, 'data' => 'Data has been saved.');
+    } catch (\Throwable $th) {
+      $response = array('status' => false, 'data' => 'Failed to save your data.');
+    };
+
+    return $response;
+  }
+
+
+  public function update($id)
+  {
+    $response = array('status' => false, 'data' => 'No operation.');
+
+    try {
+      $this->jenis_cuti = $this->input->post('jenis_cuti');
+      $this->awal_cuti = $this->input->post('awal_cuti');
+      $this->akhir_cuti = $this->input->post('akhir_cuti');
+      $this->tanggal_bekerja = $this->input->post('tanggal_bekerja');
+      $this->alamat_cuti = $this->input->post('alamat_cuti');
+      $this->telepon_cuti = $this->input->post('telepon_cuti');
+      $this->updated_by = $this->session->userdata('user')['id'];
+      $this->updated_date = date('Y-m-d H:i:s');
+      $this->db->update($this->_table, $this, array('id' => $id));
+
+      $response = array('status' => true, 'data' => 'Data has been saved.');
+    } catch (\Throwable $th) {
+      $response = array('status' => false, 'data' => 'Failed to save your data.');
+    };
+
+    return $response;
+  }
+
+  public function approve($id)
+  {
+      $response = array('status' => false, 'data' => 'No operation.');
+  
+      try {
+          $query = $this->db->select('persetujuan_pertama, persetujuan_kedua, persetujuan_ketiga, status_persetujuan')
+                            ->from($this->_table)
+                            ->where('id', $id)
+                            ->get()
+                            ->row();
+  
+          if (!$query) {
+              return array('status' => false, 'data' => 'No data found for the specified ID.');
+          }
+          $p1 = $query->persetujuan_pertama;
+          $p2 = $query->persetujuan_kedua;
+          $p3 = $query->persetujuan_ketiga;
+          $ps = $query->status_persetujuan;
+          $status = $this->input->post('persetujuan');
+          $newStatus = $status . " " . $this->session->userdata('user')['sub_unit'];
+          if ($this->session->userdata('user')['role'] === 'Administrator'){
+                if (empty($p1)) {
+                    if($status == 'Ditolak'){
+                      $this->persetujuan_pertama = $status;
+                      $this->status_persetujuan = $status;
+                    }else{
+                      $this->persetujuan_pertama = $newStatus;
+                    }
+                } elseif (!empty($p1) && empty($p2)) {
+                    if($status == 'Ditolak'){
+                      $this->persetujuan_kedua = $status;
+                      $this->status_persetujuan = $status;
+                    }else{
+                      $this->persetujuan_kedua = $newStatus;
+                    }
+                } elseif (!empty($p2) && empty($p3)) {
+                    if($status == 'Ditolak'){
+                      $this->persetujuan_ketiga = $status;
+                      $this->status_persetujuan = 'Dipertimbangkan';
+                    }else{
+                      $this->persetujuan_ketiga = $newStatus;
+                      $this->status_persetujuan = $status;
+                    }
+                } elseif ($ps=='Dipertimbangkan'){
+                  if($status == 'Ditolak'){
+                    $this->persetujuan_ketiga = $status;
+                    $this->status_persetujuan = $status;
+                  }else{
+                    $this->persetujuan_ketiga = $newStatus;
+                    $this->status_persetujuan = $status;
+                  }
+                }
+          } else {
+            if (empty($p1)) {
+                if($status == 'Ditolak'){
+                  $this->persetujuan_pertama = $status;
+                  $this->status_persetujuan = $status;
+                }else{
+                  $this->persetujuan_pertama = $newStatus;
+                }
+            } elseif (!empty($p1) && empty($p2)) {
+                if($status == 'Ditolak'){
+                  $this->persetujuan_kedua = $status;
+                  $this->status_persetujuan = $status;
+                }else{
+                  $this->persetujuan_kedua = $newStatus;
+                }
+            } elseif (!empty($p2) && empty($p3)) {
+                if($status == 'Ditolak'){
+                  $this->persetujuan_ketiga = $status;
+                  $this->status_persetujuan = 'Dipertimbangkan';
+                }else{
+                  $this->persetujuan_ketiga = $newStatus;
+                  $this->status_persetujuan = $status;
+                }
+            }
+          }
+          $this->updated_by = $this->session->userdata('user')['id'];
+          $this->updated_date = date('Y-m-d H:i:s');
+  
+          // Update the database
+          $this->db->update($this->_table, $this, array('id' => $id));
+  
+          $response = array('status' => true, 'data' => 'Data has been saved.');
+      } catch (\Throwable $th) {
+          $response = array('status' => false, 'data' => 'Failed to save your data: ' . $th->getMessage());
+      }
+  
+      return $response;
+  }
+  
+  
+
+  public function delete($id)
+  {
+    $response = array('status' => false, 'data' => 'No operation.');
+
+    try {
+      $this->db->delete($this->_table, array('id' => $id));
+
+      $response = array('status' => true, 'data' => 'Data has been deleted.');
+    } catch (\Throwable $th) {
+      $response = array('status' => false, 'data' => 'Failed to delete your data.');
+    };
+
+    return $response;
+  }
+
+  public function truncate()
+  {
+    $response = array('status' => false, 'data' => 'No operation.');
+
+    try {
+      $this->db->truncate($this->_table);
+
+      $response = array('status' => true, 'data' => 'Data has been truncated.');
+    } catch (\Throwable $th) {
+      $response = array('status' => false, 'data' => 'Failed to truncate your data.');
+    };
+
+    return $response;
+  }
+
+  function br2nl($text)
+  {
+    return str_replace("\r\n", '<br/>', htmlspecialchars_decode($text));
+  }
+
+  function clean_number($number)
+  {
+    return preg_replace('/[^0-9]/', '', $number);
+  }
+}
---
+++ application/modules/cuti/views/view-single.php
@@ -0,0 +1,114 @@
+<div class="modal fade" id="modal-view-cuti-single" data-backdrop="static" data-keyboard="false">
+    <div class="modal-dialog modal-dialog-centered modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title pull-left">
+                    <?= (isset($card_title)) ? $card_title.". Tanggal Pengajuan : ".@$cuti->tanggal_pengajuan : 'Rincian' ?>
+                </h5>
+            </div>
+            <div class="spinner">
+                <div class="lds-hourglass"></div>
+            </div>
+            <div class="modal-body">
+                <div class="x-section-group mb-3 mt-2">
+                    <span class="x-section-group-title">Data Pegawai</span>
+                    <div class="x-section-group-body">
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Pegawai</label>
+                                    <div class="form-control"><?= @$cuti->nama_lengkap ?>&nbsp;</div>
+                                </div>
+                                <div class="form-group">
+                                    <label>NRP</label>
+                                    <div class="form-control"><?= @$cuti->nrp ?>&nbsp;</div>
+                                </div>
+                                <div class="form-group">
+                                    <label>Kategori Pegawai</label>
+                                    <div class="form-control"><?= @$cuti->nama_kategori_pegawai ?>&nbsp;</div>
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Unit</label>
+                                    <div class="form-control"><?= @$cuti->nama_unit ?>&nbsp;</div>
+                                </div>
+                                <div class="form-group">
+                                    <label>Sub Unit</label>
+                                    <div class="form-control"><?= @$cuti->nama_sub_unit ?>&nbsp;</div>
+                                </div>
+                                <div class="form-group">
+                                    <label>Jabatan</label>
+                                    <div class="form-control"><?= @$cuti->nama_jabatan ?>&nbsp;</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="x-section-group mb-3 mt-2">
+                    <span class="x-section-group-title">Data Cuti</span>
+                    <div class="x-section-group-body">
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Alasan cuti</label>
+                                    <div class="form-control"><?= @$cuti->jenis_cuti ?>&nbsp;</div>
+                                </div>
+                                <div class="form-group">
+                                    <label>No. Telepon saat cuti</label>
+                                    <div class="form-control"><?= (@$cuti->telepon_cuti == '') ? @$cuti->no_hp : @$cuti->telepon_cuti ?>&nbsp;</div>
+                                    </div>
+                                <div class="form-group">
+                                    <label>Alamat saat cuti</label>
+                                    <div class="form-control"><?= (@$cuti->alamat_cuti == '') ? @$cuti->alamat_ktp : @$cuti->alamat_cuti ?>&nbsp;</div>
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Dimulai dari</label>
+                                    <div class="form-control"><?= @$cuti->awal_cuti ?>&nbsp;</div>
+                                </div>
+                                <div class="form-group">
+                                    <label>Sampai dengan</label>
+                                    <div class="form-control"><?= @$cuti->akhir_cuti ?>&nbsp;</div>
+                                </div>
+                                <div class="form-group">
+                                    <label>Bekerja kembali</label>
+                                    <div class="form-control"><?= @$cuti->tanggal_bekerja ?>&nbsp;</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="x-section-group mb-3 mt-2">
+                    <span class="x-section-group-title">Daftar Persetujuan</span>
+                    <div class="x-section-group-body">
+                        <div class="row">
+                            <div class="form-group">
+                                <label>Status Persetujuan</label>
+                                <div class="form-control"><?= ($status_persetujuan == '') ? '-' : $status_persetujuan ?>&nbsp;</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <div class="modal-footer">
+            <?php if($status_persetujuan == '') : ?>
+                <form id="form-persetujuan" autocomplete="off">
+                <input type="hidden" name="ref" value="<?= @$cuti->id ?>" readonly />
+                <input type="hidden" name="nama" value="<?= @$cuti->nama_lengkap ?>" />
+                <input type="hidden" name="persetujuan" />
+                </form>
+                <button type="button" class="btn btn-success btn--icon-text cuti-action-setuju">
+                    <i class="zmdi zmdi-check"></i> Setuju
+                </button>
+                <button type="button" class="btn btn-danger btn--icon-text cuti-action-tolak">
+                    <i class="zmdi zmdi-check"></i> Tolak
+                </button>
+            <?php endif ?>
+                <button type="button" class="btn btn-light btn--icon-text cuti-action-cancel" data-dismiss="modal">
+                    Tutup
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
