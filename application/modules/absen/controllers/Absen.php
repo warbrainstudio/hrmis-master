4293acfbf75fa66b8613a808e3470b62b00b8e05
warbrainstudio <aduystudio@gmail.com>
2024-11-21 08:31:04 +0700

    .

--- application/modules/absen/controllers/Absen.php
+++ application/modules/absen/controllers/Absen.php
@@ -177,7 +177,7 @@ class Absen extends AppBackend
       $tahun = $this->years;
       $formattedMonth = $this->get_month($bulan);
       $formattedDate = 'periode_'.$formattedMonth . '_' . $tahun;
-      $tanggal_periode = 'bulan ' . $formattedMonth . ' '.$tahun;
+      $tanggal_periode = 'periode bulan ' . $formattedMonth . ' '.$tahun;
 
       if (!is_null($master) && count($master) > 0) {
         $outputFileName = 'absensi_pegawai_'.$formattedDate.'.xlsx';
